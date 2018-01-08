c56a8f63146b2a92f3e8b271aeabce6a37fd614b
Songsmtih <33103014+Songsmtih@users.noreply.github.com>
2018-01-08 14:58:32 -0500

    Update and rename deadreckon.java to auto.java

---
+++ TestFiles/Derrick/auto.java
@@ -0,0 +1,7 @@
+// deadreckoning atonomous code
+
+package org.usfirst.frc.team5736.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive
--- TestFiles/Derrick/deadreckon.java
+++
@@ -1,5 +0,0 @@
-// deadreckoning atonomous code
-
-package org.usfirst.frc.team5736.robot;
-
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
